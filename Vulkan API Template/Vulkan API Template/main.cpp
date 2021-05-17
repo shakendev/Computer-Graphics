a18338f91f8e4299df63daf2666e796a74b4f876
Dmitry Novikov <dimka22081998@icloud.com>
2021-05-17 16:12:03 +0300

    Creating "Vulkan API Template" project

---
+++ Vulkan API Template/Vulkan API Template/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Vulkan API Template
+//
+//  Created by Dimka Novikov on 17.05.2021.
+//
+
+#include <iostream>
+
+
+
+int main(int argc, char *argv[]) {
+    
+    
+    
+    return EXIT_SUCCESS;
+}
